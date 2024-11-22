08c75104a4f0df7a14a3c4ab0110b9a3c5aee7a0
JesusHTML <jesuslopezfernandez2207@gmail.com>
2024-11-22 13:00:23 +0100

    version3

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Post # Con esto importo la clase Post de models.py
+
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    cuerpo = models.TextField()
+
+    def __str__(self):
+        return f"Nombre post: {self.titulo} , id {self.id}"
