b812a9dc6f0316845c52f007e4f9cb1fe8ce1bc3
JesusHTML <jesuslopezfernandez2207@gmail.com>
2024-11-27 09:42:07 +0100

    lunes27-nov

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=200)
     cuerpo = models.TextField()
+    fechasP = models.DateField()
 
     def __str__(self):
         return f"Nombre post: {self.titulo} , id {self.id}"
