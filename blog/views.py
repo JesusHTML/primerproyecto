1bc7cf6573ca98bd843b366bd24dd8c659954a94
JesusHTML <jesuslopezfernandez2207@gmail.com>
2024-11-27 13:10:30 +0100

    version4

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def principal(request):  # Siempre se pasa como parametro request
-    return render(request, 'blog/principal.html')  # render renderiza 
-                #peticion    #archivo
+    posts = Post.objects.all()
+    autores = Post.objects.values_list('autor', flat=True).distinct() # Con esto vamos a cojer a todos los autores pero no repetiremos sus nombres
+    return render(request, 'blog/principal.html' , {'posts':posts , 'autores':autores})       # render renderiza 
+                #peticion         #archivo             #Creo un dicionario
+
+
+
+
 
 # En templates crearemos los html
